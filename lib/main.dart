da38b11db8727023fe9b92215a32664d9d415ba6
Lakshika Rasanjali <62471933+Lakshika-Luck@users.noreply.github.com>
2020-10-29 21:34:35 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,288 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          brightness: Brightness.light,
+          primaryColor: Colors.blueAccent,
+          accentColor: Colors.grey),
+      home: MyApp(),
+    ));
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String studentName, studentIndex, studentCourse;
+  double studentGPA;
+
+  getStudentName(name) {
+    this.studentName = name;
+  }
+
+  getStudentIndex(index) {
+    this.studentIndex = index;
+  }
+
+  getStudentCourse(course) {
+    this.studentCourse = course;
+  }
+
+  getStudentGPA(gpa) {
+    this.studentGPA = double.parse(gpa);
+  }
+
+  createData() {
+    DocumentReference documentReference =
+        Firestore.instance.collection("MyStudents").document(studentName);
+
+    //create map
+    Map<String, dynamic> students = {
+      "studentName": studentName,
+      "studentIndex": studentIndex,
+      "studentCourse": studentCourse,
+      "studentGPA": studentGPA
+    };
+
+    //send mapping to doceumnt reference
+    documentReference.setData(students).whenComplete(() {
+      print("$studentName created");
+    });
+  }
+
+  readData() {
+    DocumentReference documentReference =
+        Firestore.instance.collection("MyStudents").document(studentName);
+
+    documentReference.get().then((datasnapshot) {
+      print(datasnapshot.data["studentName"]);
+      print(datasnapshot.data["studentIndex"]);
+      print(datasnapshot.data["studentCourse"]);
+      print(datasnapshot.data["studentGPA"]);
+    });
+  }
+
+  updateData() {
+    DocumentReference documentReference =
+        Firestore.instance.collection("MyStudents").document(studentName);
+
+    //create map
+    Map<String, dynamic> students = {
+      "studentName": studentName,
+      "studentIndex": studentIndex,
+      "studentCourse": studentCourse,
+      "studentGPA": studentGPA
+    };
+
+    //send mapping to doceumnt reference
+    documentReference.setData(students).whenComplete(() {
+      print("$studentName updated");
+    });
+  }
+
+  deleteData() {
+    DocumentReference documentReference =
+        Firestore.instance.collection("MyStudents").document(studentName);
+    documentReference.delete().whenComplete(() {
+      print("$studentName Removed");
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+          title: Text("E College Enrollment"),
+        ),
+        body: Column(children: <Widget>[
+          Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 43.0),
+              child: TextFormField(
+                decoration: InputDecoration(
+                    labelText: "Name",
+                    fillColor: Colors.white,
+                    focusedBorder: OutlineInputBorder(
+                        borderSide:
+                            BorderSide(color: Colors.blue, width: 2.0))),
+                onChanged: (String name) {
+                  getStudentName(name);
+                },
+              )),
+          Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 43.0),
+              child: TextFormField(
+                decoration: InputDecoration(
+                    labelText: "Student Index",
+                    fillColor: Colors.white,
+                    focusedBorder: OutlineInputBorder(
+                        borderSide:
+                            BorderSide(color: Colors.blue, width: 2.0))),
+                onChanged: (String index) {
+                  getStudentIndex(index);
+                },
+              )),
+          Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 43.0),
+              child: TextFormField(
+                decoration: InputDecoration(
+                    labelText: "Course",
+                    fillColor: Colors.white,
+                    focusedBorder: OutlineInputBorder(
+                        borderSide:
+                            BorderSide(color: Colors.blue, width: 2.0))),
+                onChanged: (String course) {
+                  getStudentCourse(course);
+                },
+              )),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 43.0),
+            child: TextFormField(
+              decoration: InputDecoration(
+                  labelText: "GPA",
+                  fillColor: Colors.white,
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.blue, width: 2.0))),
+              onChanged: (String gpa) {
+                getStudentGPA(gpa);
+              },
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 20),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30.0),
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    new SizedBox(
+                      width: 60,
+                      height: 30,
+                      child: RaisedButton(
+                        color: Colors.blueGrey,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(16)),
+                        child: Text("Add"),
+                        textColor: Colors.white,
+                        onPressed: () {
+                          createData();
+                        },
+                      ),
+                    ),
+                    new SizedBox(
+                      width: 76,
+                      height: 30,
+                      child: RaisedButton(
+                        color: Colors.blueGrey,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(16)),
+                        child: Text("Details"),
+                        textColor: Colors.white,
+                        onPressed: () {
+                          readData();
+                        },
+                      ),
+                    ),
+                    new SizedBox(
+                      width: 78,
+                      height: 30,
+                      child: RaisedButton(
+                        color: Colors.blueGrey,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(16)),
+                        child: Text("Update"),
+                        textColor: Colors.white,
+                        onPressed: () {
+                          updateData();
+                        },
+                      ),
+                    ),
+                    new SizedBox(
+                        width: 83,
+                        height: 30,
+                        child: RaisedButton(
+                          color: Colors.blueGrey,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(16)),
+                          child: Text("Remove"),
+                          textColor: Colors.white,
+                          onPressed: () {
+                            deleteData();
+                          },
+                        )),
+                  ]),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 44.0),
+            child: Padding(
+              padding: const EdgeInsets.only(top: 20, left: 1, right: 1),
+              child: Row(
+                //textDirection: TextDirection.ltr,
+                children: <Widget>[
+                  Expanded(
+                    child: Text("Name"),
+                  ),
+                  Expanded(
+                    child: Text("Index"),
+                  ),
+                  Expanded(
+                    child: Text("Course"),
+                  ),
+                  Expanded(
+                    child: Text("GPA"),
+                  )
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 44.0),
+            child: Padding(
+                padding: const EdgeInsets.only(top: 15, left: 1, right: 5),
+                child: StreamBuilder(
+                    stream:
+                        Firestore.instance.collection("MyStudents").snapshots(),
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        return ListView.builder(
+                            shrinkWrap: true,
+                            itemCount: snapshot.data.documents.length,
+                            itemBuilder: (context, index) {
+                              DocumentSnapshot documentSnapshot =
+                                  snapshot.data.documents[index];
+                              return Row(
+                                children: <Widget>[
+                                  Expanded(
+                                    child:
+                                        Text(documentSnapshot["studentName"]),
+                                  ),
+                                  Expanded(
+                                    child:
+                                        Text(documentSnapshot["studentIndex"]),
+                                  ),
+                                  Expanded(
+                                    child:
+                                        Text(documentSnapshot["studentCourse"]),
+                                  ),
+                                  Expanded(
+                                    child: Text(documentSnapshot["studentGPA"]
+                                        .toString()),
+                                  )
+                                ],
+                              );
+                            });
+                      } else {
+                        return Align(
+                          alignment: FractionalOffset.bottomCenter,
+                          child: CircularProgressIndicator(),
+                        );
+                      }
+                    })),
+          ),
+        ]));
+  }
+}
